6fb8ed01c042d294740d5ddb0aad285b7485ba18
李嘉俊 <2383690354@qq.com>
2024-11-22 20:41:14 +0800

    加入docker支持

--- BookSpring.WebApp/Program.cs
+++ BookSpring.WebApp/Program.cs
@@ -10,15 +10,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-var endpoint = "https://miniocon.hkg1.zeabur.app/";
-var accessKey = "fwjUZer4vuPLXeXkiO1I";
-var secretKey = "lJgRGl0QO3gzlbJJdOke9Ifr2oggSEOVzSuw0Bc1";
+const string endpoint = "miniocon.hkg1.zeabur.app";
+const string accessKey = "fwjUZer4vuPLXeXkiO1I";
+const string secretKey = "lJgRGl0QO3gzlbJJdOke9Ifr2oggSEOVzSuw0Bc1";
 
 builder.Services.AddMinio(configureClient => configureClient
-            .WithEndpoint(endpoint)
-            .WithCredentials(accessKey, secretKey)
-            .WithSSL()
-            .Build());
+    .WithEndpoint(endpoint)
+    .WithCredentials(accessKey, secretKey)
+    .WithSSL());
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
